e407082395f320901ac0d372501b273cba9decdd
allanpatrick18 <allanpatrick18@gmail.com>
2017-11-30 20:53:36 -0200

    testes

--- ProjectFiles/Blinky.c
+++ ProjectFiles/Blinky.c
@@ -17,7 +17,11 @@
 #include "driverlib/rom.h"
 #include "driverlib/sysctl.h"
 #include "grlib/grlib.h"
+#include "driverlib/pin_map.h"
 #include "cfaf128x128x16.h"
+#include "driverlib/i2c.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
 
 osThreadId tid_phaseA;                  /* Thread id of thread: phase_a      */
 osThreadId tid_phaseB;                  /* Thread id of thread: phase_b      */
@@ -31,7 +35,114 @@ osThreadId tid_clock;                   /* Thread id of thread: clock        */
 #define LED_D      3
 #define LED_CLK    7
 
+//Buffer para impressão de strings no LCD
+char buf [10] ;
+char buf1 [10] ;
+//Converte um numero em uma base quanlquer para uma string de caracteres imprimiveis
 
+static void intToString(int64_t value, char * pBuf, uint32_t len, uint32_t base){
+    static const char* pAscii = "0123456789abcdefghijklmnopqrstuvwxyz";
+    int pos = 0;
+    int64_t tmpValue = value;
+
+    // the buffer must not be null and at least have a length of 2 to handle one
+    // digit and null-terminator
+    if (pBuf == NULL || len < 2)
+    {
+        return;
+    }
+
+    // a valid base cannot be less than 2 or larger than 36
+    // a base value of 2 means binary representation. A value of 1 would mean only zeros
+    // a base larger than 36 can only be used if a larger alphabet were used.
+    if (base < 2 || base > 36)
+    {
+        return;
+    }
+
+    // negative value
+    if (value < 0)
+    {
+        tmpValue = -tmpValue;
+        value    = -value;
+        pBuf[pos++] = '-';
+    }
+
+    // calculate the required length of the buffer
+    do {
+        pos++;
+        tmpValue /= base;
+    } while(tmpValue > 0);
+
+
+    if (pos > len)
+    {
+        // the len parameter is invalid.
+        return;
+    }
+
+    pBuf[pos] = '\0';
+
+    do {
+        pBuf[--pos] = pAscii[value % base];
+        value /= base;
+    } while(value > 0);
+
+    return;
+}
+
+void initI2C0(void)
+{
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_I2C0);
+
+   //reset I2C module
+   SysCtlPeripheralReset(SYSCTL_PERIPH_I2C0);
+
+   //enable GPIO peripheral that contains I2C
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+   // Configure the pin muxing for I2C0 functions on port B2 and B3.
+   GPIOPinConfigure(GPIO_PB2_I2C0SCL);
+   GPIOPinConfigure(GPIO_PB3_I2C0SDA);
+
+   // Select the I2C function for these pins.
+   GPIOPinTypeI2CSCL(GPIO_PORTB_BASE, GPIO_PIN_2);
+   GPIOPinTypeI2C(GPIO_PORTB_BASE, GPIO_PIN_3);
+
+   // Enable and initialize the I2C0 master module.  Use the system clock for
+   // the I2C0 module.  The last parameter sets the I2C data transfer rate.
+   // If false the data rate is set to 100kbps and if true the data rate will
+   // be set to 400kbps.
+   I2CMasterInitExpClk(I2C0_BASE, SysCtlClockGet(), false);
+
+}
+
+uint8_t readI2C0(uint16_t device_address, uint16_t device_register)
+{
+	//specify that we want to communicate to device address with an intended write to bus
+	I2CMasterSlaveAddrSet(I2C0_BASE, device_address, false);
+
+	//the register to be read
+	I2CMasterDataPut(I2C0_BASE, device_register);
+
+	//send control byte and register address byte to slave device
+	I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_SEND);
+
+	//wait for MCU to complete send transaction
+	while(I2CMasterBusy(I2C0_BASE));
+
+	//read from the specified slave device
+	I2CMasterSlaveAddrSet(I2C0_BASE, device_address, true);
+
+	//send control byte and read from the register from the MCU
+	I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_SINGLE_RECEIVE);
+
+	//wait while checking for MCU to complete the transaction
+	while(I2CMasterBusy(I2C0_BASE));
+
+	//Get the data from the MCU register and return to caller
+	return( I2CMasterDataGet(I2C0_BASE));
+}
 
 /*----------------------------------------------------------------------------
  *      Switch LED on
@@ -132,52 +243,63 @@ osThreadDef(clock,  osPriorityNormal, 1, 0);
  *      Main: Initialize and start RTX Kernel
  *---------------------------------------------------------------------------*/
 int main (void) {
+	
+	
 	tContext sContext;
 	tRectangle sRect;
 	
 //	ROM_SysCtlDeepSleepClockConfigSet(16, SYSCTL_DSLP_OSC_INT);
 //	SysCtlDeepSleepPowerSet(0x121);  // TSPD, FLASHPM = LOW_POWER_MODE, SRAMPM = STANDBY_MODE
-
+	uint8_t teste , teste1;
 	osKernelInitialize();
 	
   SystemCoreClockUpdate();
   LED_Initialize();                         /* Initialize the LEDs           */
  	cfaf128x128x16Init();
 	
-	GrContextInit(&sContext, &g_sCfaf128x128x16);
 	
-  tid_phaseA = osThreadCreate(osThread(phaseA), NULL);
-  tid_phaseB = osThreadCreate(osThread(phaseB), NULL);
-  tid_phaseC = osThreadCreate(osThread(phaseC), NULL);
-  tid_phaseD = osThreadCreate(osThread(phaseD), NULL);
-  tid_clock  = osThreadCreate(osThread(clock),  NULL);
+	initI2C0();
+	GrContextInit(&sContext, &g_sCfaf128x128x16);
+	GrContextFontSet(&sContext, g_psFontCm12);
+	while(1){
+	
+	teste = readI2C0(0x40,0x00);
+	teste1 = readI2C0(0x40,0x01);
+	intToString(teste, buf, 10, 10);
+	intToString(teste1, buf1, 10, 10);
+	
+//  tid_phaseA = osThreadCreate(osThread(phaseA), NULL);
+//  tid_phaseB = osThreadCreate(osThread(phaseB), NULL);
+//  tid_phaseC = osThreadCreate(osThread(phaseC), NULL);
+//  tid_phaseD = osThreadCreate(osThread(phaseD), NULL);
+//  tid_clock  = osThreadCreate(osThread(clock),  NULL);
 
-	osKernelStart();
+	//osKernelStart();
 	
 	sRect.i16XMin = 0;
 	sRect.i16YMin = 0;
 	sRect.i16XMax = GrContextDpyWidthGet(&sContext) - 1;
-	sRect.i16YMax = 23;
+	sRect.i16YMax = 36;
+		
 	GrContextForegroundSet(&sContext, ClrDarkBlue);
 	GrRectFill(&sContext, &sRect);
-
+		
 	GrContextForegroundSet(&sContext, ClrWhite);
 	GrRectDraw(&sContext, &sRect);
-
-	GrContextFontSet(&sContext, g_psFontCm12);
-	GrStringDrawCentered(&sContext, "hello", -1,
+	
+	GrStringDrawCentered(&sContext, buf, -1,
 											 GrContextDpyWidthGet(&sContext) / 2, 10, 0);
-
-	GrContextFontSet(&sContext, g_psFontCm12/*g_psFontFixed6x8*/);
-	GrStringDrawCentered(&sContext, "Hello World!", -1,
-											 GrContextDpyWidthGet(&sContext) / 2,
-											 ((GrContextDpyHeightGet(&sContext) - 24) / 2) + 24,
-											 0);
-
+	GrStringDrawCentered(&sContext, buf, -1,
+											 GrContextDpyWidthGet(&sContext) / 2, 24, 0);
 	GrFlush(&sContext);
 	
-	osSignalSet(tid_phaseA, 0x0001);          /* set signal to phaseA thread   */
+	SysCtlDelay(65535);
+	SysCtlDelay(65535);
+	SysCtlDelay(65535);
+}
 	
-  osDelay(osWaitForever);
-  while(1);
+//	osSignalSet(tid_phaseA, 0x0001);          /* set signal to phaseA thread   */
+//	
+//  osDelay(osWaitForever);
+//  while(1);
 }
