52e4ff53b7fa942b7b62ce9853713c905ab52e39
Jessica Isoton <jessicasampaio195@gmail.com>
2017-12-04 15:01:43 -0200

    20171204

--- ProjectFiles/Blinky.c
+++ ProjectFiles/Blinky.c
@@ -23,6 +23,9 @@
 #include "C:\ti\TivaWare_C_Series-2.1.4.178\utils\uartstdio.h"
 #include "cfaf128x128x16.h"
 #include "math.h"
+#include <stdint.h>
+#include "TM4C129.h"
+
 
 static void intToString(int64_t value, char* pBuf, uint32_t len, uint32_t base)
 {
@@ -77,27 +80,33 @@ static void intToString(int64_t value, char* pBuf, uint32_t len, uint32_t base)
 
 }
 
+
+
 int main (void) {
 		//Impressao dos valores do ADC
 		tContext sContext;
 		tRectangle sRect;
 			char pBuf[10];
 	uint32_t ui32SysClock;
+	int buttonpressed = 0;
+int32_t i32Valx, i32Valy;
+
+ cfaf128x128x16Init();
+		//Inicializacoes de Contexto
+	  GrContextInit(&sContext, &g_sCfaf128x128x16);
+		GrContextForegroundSet(&sContext, ClrWhite);
+		GrRectDraw(&sContext, &sRect);
+		GrContextFontSet(&sContext, g_psFontCm12/*g_psFontFixed6x8*/);	
 	
-int32_t i32Val;
 	
-			ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
-                                       SYSCTL_OSC_MAIN |
-                                       SYSCTL_USE_PLL |
-                                       SYSCTL_CFG_VCO_480), 120000000);
 //
 // Enable the GPIOA peripheral
 //
-SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 //
 // Wait for the GPIOA module to be ready.
 //
-while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
+while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOC))
 {
 }
 //
@@ -105,14 +114,13 @@ while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
 // level interrupt handler for all the pin interrupts.
 //
 
-GPIOIntRegister(GPIO_PORTE_BASE, GPIOE_Handler);
+//GPIOIntRegister(GPIO_PORTC_BASE, GPIOC_Handler);
 //
 // Initialize the GPIO pin configuration.
 //
 // Set pins 2, 4, and 5 as input, SW controlled.
 //
-GPIOPinTypeGPIOInput(GPIO_PORTE_BASE,
-GPIO_PIN_3);
+GPIOPinTypeGPIOInput(GPIO_PORTC_BASE,GPIO_PIN_6);
 //
 // Set pins 0 and 3 as output, SW controlled.
 //
@@ -120,7 +128,7 @@ GPIO_PIN_3);
 //
 // Make pins 2 and 4 rising edge triggered interrupts.
 //
-GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_3, GPIO_RISING_EDGE);
+GPIOIntTypeSet(GPIO_PORTC_BASE, GPIO_PIN_6, GPIO_HIGH_LEVEL);
 //
 // Make pin 5 high level triggered interrupts.
 //
@@ -128,7 +136,8 @@ GPIOIntTypeSet(GPIO_PORTE_BASE, GPIO_PIN_3, GPIO_RISING_EDGE);
 //
 // Read some pins.
 //
-i32Val = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_3);
+i32Valx = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_6);
+
 //
 // Write some pins. Even though pins 2, 4, and 5 are specified, those pins
 // are unaffected by this write because they are configured as inputs. At
@@ -142,13 +151,23 @@ i32Val = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_3);
 //
 // Enable the pin interrupts.
 //
-GPIOIntEnable(GPIO_PORTE_BASE, GPIO_PIN_3);
+GPIOIntEnable(GPIO_PORTC_BASE, GPIO_PIN_6);
 
 while(1)
 {
-
-i32Val = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_3);
 	
+	i32Valx = 0x00;
+
+i32Valx = GPIOPinRead(GPIO_PORTC_BASE, GPIO_PIN_6);
+
+	buttonpressed = 0;
+	
+	if(i32Valx == 0)
+	{
+		buttonpressed = 1;
+	}
+	
+	if(buttonpressed == 1){
 		sRect.i16XMin = 0;
 			sRect.i16YMin = 0;
 			sRect.i16XMax = GrContextDpyWidthGet(&sContext) -1;
@@ -160,7 +179,7 @@ i32Val = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_3);
 			GrRectDraw(&sContext, &sRect);
 
 			
-			intToString(i32Val/*converted_Value*/, pBuf, 10, 10);		
+			intToString(1, pBuf, 10, 10);		
 			
 			//VREFP - VREFN = 3.3
 	
@@ -168,9 +187,15 @@ GrStringDrawCentered(&sContext, pBuf, 10,
 											 GrContextDpyWidthGet(&sContext) / 2,
 											 ((GrContextDpyHeightGet(&sContext) - 84) / 2) + 24,
 											 0);
-intToString(i32Val/*converted_Value*/, pBuf, 10, 10);		
+intToString(i32Valx/*converted_Value*/, pBuf, 10, 10);		
+	}
+		sRect.i16XMin = 0;
+		sRect.i16YMin = 0;
+			sRect.i16XMax = GrContextDpyWidthGet(&sContext) -1;
+			sRect.i16YMax = GrContextDpyHeightGet(&sContext) -1;
+			GrContextForegroundSet(&sContext, ClrBlack);
+			GrRectFill(&sContext, &sRect);
 
-		SysCtlDelay(ui32SysClock / 12);
 }
 
 }
